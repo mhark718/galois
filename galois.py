ac3a0ea9023ffb8b352fac01ab4914a035b54853
mhark718 <mhark718@users.noreply.github.com>
2022-07-28 13:43:44 -0700

    Add files via upload

--- galois.py
+++ galois.py
@@ -1,6 +1,11 @@
-def f(j,base=7):
-	return (2*j*j+j+3)%base
-poly=(list(range(7)))
+base=7
+exponent=2
+coefficents=[3,1,0,2]
+#def f(j,base=7):
+#	return (2*j*j+j+3)%base
+def f(j,thebase):
+	return (2*j*j+j+1)%thebase
+poly=(list(range(base)))
 print (poly)
-res=[f(i) for i in poly]
+res=[f(i,base) for i in poly]
 print (res)
